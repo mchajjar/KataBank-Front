dc092f34d2da996bc1204074e9230712bb19e681
Mchajjar <chhajjar2006@gmail.com>
2022-10-02 19:59:32 +0200

    add empty services

---
+++ KataBankFront/src/app/Services/operation-service/operation-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operation-service',
+  templateUrl: './operation-service.component.html',
+  styleUrls: ['./operation-service.component.css']
+})
+export class OperationServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- KataBankFront/src/app/app.module.ts
+++ KataBankFront/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserServiceComponent } from './services/user-service/user-service.component';
+import { AccountServiceComponent } from './services/account-service/account-service.component';
+import { OperationServiceComponent } from './services/operation-service/operation-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserServiceComponent,
+    AccountServiceComponent,
+    OperationServiceComponent
   ],
   imports: [
     BrowserModule,
