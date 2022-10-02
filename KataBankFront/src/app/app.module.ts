3737f7567e1fe3f435fc62b84b3d3b0e51b2c70f
chedly hajjar <chhajjar2006@gmail.com>
2022-10-02 18:04:28 +0000

    Develop

--- KataBankFront/src/app/app.module.ts
+++ KataBankFront/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { UserServiceComponent } from './services/user-service/user-service.component';
 import { AccountServiceComponent } from './services/account-service/account-service.component';
 import { OperationServiceComponent } from './services/operation-service/operation-service.component';
+import { AccountComponent } from './account/account/account.component';
+import { OperationComponent } from './operation/operation/operation.component';
+import { UserComponent } from './user/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserServiceComponent,
     AccountServiceComponent,
-    OperationServiceComponent
+    OperationServiceComponent,
+    AccountComponent,
+    OperationComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
